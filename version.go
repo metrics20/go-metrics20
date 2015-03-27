acb4ae14d35b3312d73cb774026a27fe3b4350f5
Dieter Plaetinck <dieter@plaetinck.be>
2015-03-26 22:27:23 -0400

    byte versions of metric20 functions, better validation

--- version.go
+++ version.go
@@ -1,6 +1,7 @@
 package metrics20
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"strings"
@@ -34,20 +35,56 @@ func GetVersion(metric_in string) metricVersion {
 	return Legacy
 }
 
+var byteEquals = []byte("=")
+var byteIs = []byte("_is_")
+
+// getVersionB is like getVersion but for byte array input.
+func GetVersionB(metric_in []byte) metricVersion {
+	if bytes.Contains(metric_in, byteEquals) {
+		return M20
+	}
+	if bytes.Contains(metric_in, byteIs) {
+		return M20NoEquals
+	}
+	return Legacy
+}
+
 func IsMetric20(metric_in string) bool {
-	v := getVersion(metric_in)
+	v := GetVersion(metric_in)
 	return v == M20 || v == M20NoEquals
+}
+
+// ValidateSensibleChars checks that the metric id onlay contains characters that
+// are commonly understood to be sensible and useful.  Because Graphite will do
+// the weirdest things with all kinds of special characters.
+func ValidateSensibleChars(metric_id string) error {
+	for _, ch := range metric_id {
+		if !(ch >= 'a' && ch <= 'z') && !(ch >= 'A' && ch <= 'Z') && ch != '_' && ch != '-' && ch != '.' {
+			return fmt.Errorf("metric '%s' contains illegal char '%s'", metric_id, string(ch))
+		}
+	}
+	return nil
+}
+
+// ValidateSensibleCharsB is like ValidateSensibleChars but for byte array inputs.
+func ValidateSensibleCharsB(metric_id []byte) error {
+	for _, ch := range metric_id {
+		if !(ch >= 'a' && ch <= 'z') && !(ch >= 'A' && ch <= 'Z') && ch != '_' && ch != '-' && ch != '.' {
+			return fmt.Errorf("metric '%s' contains illegal char '%s'", string(metric_id), string(ch))
+		}
+	}
+	return nil
 }
 
 // InitialValidation checks the basic form of metric keys
 func InitialValidation(metric_id string, version metricVersion) error {
 	if version == Legacy {
-		// if the metric contains no = or _is_, we don't really care what it does contain.  it can be whatever.
-		// except for:
+		// if the metric contains no = or _is_, in theory we don't really care what it does contain.  it can be whatever.
+		// in practice, graphite alters (removes a dot) the metric id when this happens:
 		if strings.Contains(metric_id, "..") {
 			return fmt.Errorf("metric '%s' has an empty node", metric_id)
 		}
-		return nil
+		return ValidateSensibleChars(metric_id)
 	}
 	if version == M20 {
 		if strings.Contains(metric_id, "_is_") {
@@ -71,6 +108,58 @@ func InitialValidation(metric_id string, version metricVersion) error {
 		}
 	}
 	if strings.Count(metric_id, ".") < 2 {
+		return fmt.Errorf("metric '%s': must have at least one tag_k/tag_v pair beyond unit and target_type", metric_id)
+	}
+	return nil
+}
+
+// optimization so compiler doesn't initialize and allocate new variables every time we use this.
+// shouldn't be needed for the strings above because they are immutable, I'm assuming the compiler optimizes for that
+var (
+	doubleDot    = []byte("..")
+	m20Is        = []byte("_is_")
+	m20UnitPre   = []byte("unit=")
+	m20UnitMid   = []byte(".unit=")
+	m20TTPre     = []byte("target_type=")
+	m20TTMid     = []byte(".target_type=")
+	m20NEIS      = []byte("=")
+	m20NEUnitPre = []byte("unit_is_")
+	m20NEUnitMid = []byte(".unit_is_")
+	m20NETTPre   = []byte("target_type_is_")
+	m20NETTMid   = []byte(".target_type_is_")
+	dot          = []byte(".")
+)
+
+// InitialValidationB is like InitialValidation but for byte array inputs.
+func InitialValidationB(metric_id []byte, version metricVersion) error {
+	if version == Legacy {
+		if bytes.Contains(metric_id, doubleDot) {
+			return fmt.Errorf("metric '%s' has an empty node", metric_id)
+		}
+		return ValidateSensibleCharsB(metric_id)
+	}
+	if version == M20 {
+		if bytes.Contains(metric_id, m20Is) {
+			return fmt.Errorf("metric '%s' has both = and _is_", metric_id)
+		}
+		if !bytes.HasPrefix(metric_id, m20UnitPre) && !bytes.Contains(metric_id, m20UnitMid) {
+			return fmt.Errorf("metric '%s' has no unit tag", metric_id)
+		}
+		if !bytes.HasPrefix(metric_id, m20TTPre) && !bytes.Contains(metric_id, m20TTMid) {
+			return fmt.Errorf("metric '%s' has no target_type tag", metric_id)
+		}
+	} else { //version == M20NoEquals
+		if bytes.Contains(metric_id, m20NEIS) {
+			return fmt.Errorf("metric '%s' has both = and _is_", metric_id)
+		}
+		if !bytes.HasPrefix(metric_id, m20NEUnitPre) && !bytes.Contains(metric_id, m20NEUnitMid) {
+			return fmt.Errorf("metric '%s' has no unit tag", metric_id)
+		}
+		if !bytes.HasPrefix(metric_id, m20NETTPre) && !bytes.Contains(metric_id, m20NETTMid) {
+			return fmt.Errorf("metric '%s' has no target_type tag", metric_id)
+		}
+	}
+	if bytes.Count(metric_id, dot) < 2 {
 		return fmt.Errorf("metric '%s': must have at least one tag_k/tag_v pair beyond unit and target_type", metric_id)
 	}
 	return nil
