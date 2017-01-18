6e880ed6742d76b6f3fd1719d50073e3c81c9086
Dieter Plaetinck <dieter@raintank.io>
2017-01-18 22:04:38 +0100

    allow for prefixes for metrics2.0 messages too
    * sometimes you want to add tags to metrics2.0 keys.
      while in theory a metrics2.0 tagset is a set of unordered keys,
      in practice we still use a string for it, especially with common
      tools (grafana, bosun, etc) that query graphite, the string concept
      matters, even for carbon2.0, so we can use the prefix to add tags.
    * attempt to make DeriveCount a bit faster for metrics2.0 data, which
      didn't seem to do much
    
    BEFORE:
    ~/g/s/g/m/g/carbon20 ❯❯❯ go test -bench .
    BenchmarkDeriveCountsM20Bare-8             	20000000	        74.8 ns/op
    BenchmarkDeriveCountsM20Proper-8           	 3000000	       543 ns/op
    BenchmarkDeriveCountsM20NoEqualsBare-8     	20000000	        89.2 ns/op
    BenchmarkDeriveCountsM20NoEqualsProper-8   	 2000000	       652 ns/op
    BenchmarkValidatePacketNone-8              	 1000000	      1148 ns/op
    BenchmarkValidatePacketMedium-8            	 1000000	      1165 ns/op
    BenchmarkValidatePacketStrict-8            	 1000000	      1253 ns/op
    PASS
    ok  	github.com/metrics20/go-metrics20/carbon20	14.924s
    
    AFTER:
    ~/g/s/g/m/g/carbon20 ❯❯❯ go test -bench .
    BenchmarkDeriveCountsM20Bare-8             	20000000	       117 ns/op
    BenchmarkDeriveCountsM20Proper-8           	 3000000	       549 ns/op
    BenchmarkDeriveCountsM20NoEqualsBare-8     	10000000	       128 ns/op
    BenchmarkDeriveCountsM20NoEqualsProper-8   	 2000000	       618 ns/op
    BenchmarkValidatePacketNone-8              	 1000000	      1133 ns/op
    BenchmarkValidatePacketMedium-8            	 1000000	      1154 ns/op
    BenchmarkValidatePacketStrict-8            	 1000000	      1232 ns/op
    PASS
    ok  	github.com/metrics20/go-metrics20/carbon20	16.535s

--- carbon20/manipulate_test.go
+++ carbon20/manipulate_test.go
@@ -9,105 +9,98 @@ import (
 var out string
 
 type Case struct {
-	in  string
-	out string
+	in   string
+	p1   string
+	p2   string
+	p2ne string
+	out  string
 }
 
 func TestDeriveCount(t *testing.T) {
-	// metrics 2.0 cases with equals
 	cases := []Case{
-		Case{"foo.bar.unit=yes.baz", "foo.bar.unit=yesps.baz"},
-		Case{"foo.bar.unit=yes", "foo.bar.unit=yesps"},
-		Case{"unit=yes.foo.bar", "unit=yesps.foo.bar"},
-		Case{"mtype=count.foo.unit=ok.bar", "mtype=rate.foo.unit=okps.bar"},
-	}
-	for _, c := range cases {
-		assert.Equal(t, DeriveCount(c.in, "prefix.", false), c.out)
-	}
+		// metrics 2.0 cases with equals
+		Case{"foo.bar.unit=yes.baz", "prefix.", "", "ignored", "foo.bar.unit=yesps.baz"},
+		Case{"foo.bar.unit=yes", "prefix.", "our=prefix.", "ignored", "our=prefix.foo.bar.unit=yesps"},
+		Case{"unit=yes.foo.bar", "prefix.", "", "ignored", "unit=yesps.foo.bar"},
+		Case{"mtype=count.foo.unit=ok.bar", "prefix.", "", "ignored", "mtype=rate.foo.unit=okps.bar"},
 
-	// same but with equals
-	for i, c := range cases {
-		cases[i] = Case{
-			strings.Replace(c.in, "=", "_is_", -1),
-			strings.Replace(c.out, "=", "_is_", -1),
-		}
+		// metrics 2.0 cases without equals
+		Case{"foo.bar.unit_is_yes.baz", "prefix.", "ignored", "", "foo.bar.unit_is_yesps.baz"},
+		Case{"foo.bar.unit_is_yes", "prefix.", "ignored", "our_is_prefix.", "our_is_prefix.foo.bar.unit_is_yesps"},
+		Case{"unit_is_yes.foo.bar", "prefix.", "ignored", "", "unit_is_yesps.foo.bar"},
+		Case{"mtype_is_count.foo.unit_is_ok.bar", "prefix.", "ignored", "", "mtype_is_rate.foo.unit_is_okps.bar"},
 	}
 	for _, c := range cases {
-		assert.Equal(t, DeriveCount(c.in, "prefix.", false), c.out)
+		assert.Equal(t, DeriveCount(c.in, c.p1, c.p2, c.p2ne, false), c.out)
 	}
 }
 
 // only 1 kind of stat is enough, cause they all behave the same
 func TestStat(t *testing.T) {
 	cases := []Case{
-		Case{"foo.bar.unit=yes.baz", "foo.bar.unit=yes.baz.stat=max_90"},
-		Case{"foo.bar.unit=yes", "foo.bar.unit=yes.stat=max_90"},
-		Case{"unit=yes.foo.bar", "unit=yes.foo.bar.stat=max_90"},
-		Case{"mtype=count.foo.unit=ok.bar", "mtype=count.foo.unit=ok.bar.stat=max_90"},
+		// metrics 2.0 cases with equals
+		Case{"foo.bar.unit=yes.baz", "prefix.", "", "ignored", "foo.bar.unit=yes.baz.stat=max_90"},
+		Case{"foo.bar.unit=yes", "prefix.", "our=prefix.", "ignored", "our=prefix.foo.bar.unit=yes.stat=max_90"},
+		Case{"unit=yes.foo.bar", "prefix.", "", "ignored", "unit=yes.foo.bar.stat=max_90"},
+		Case{"mtype=count.foo.unit=ok.bar", "prefix.", "", "ignored", "mtype=count.foo.unit=ok.bar.stat=max_90"},
+		// metrics 2.0 cases without equals
+		Case{"foo.bar.unit_is_yes.baz", "prefix.", "ignored", "", "foo.bar.unit_is_yes.baz.stat_is_max_90"},
+		Case{"foo.bar.unit_is_yes", "prefix.", "ignored", "our_is_prefix.", "our_is_prefix.foo.bar.unit_is_yes.stat_is_max_90"},
+		Case{"unit_is_yes.foo.bar", "prefix.", "ignored", "", "unit_is_yes.foo.bar.stat_is_max_90"},
+		Case{"mtype_is_count.foo.unit_is_ok.bar", "prefix.", "ignored", "", "mtype_is_count.foo.unit_is_ok.bar.stat_is_max_90"},
 	}
 	for _, c := range cases {
-		assert.Equal(t, Max(c.in, "prefix.", "90", ""), c.out)
+		assert.Equal(t, Max(c.in, c.p1, c.p2, c.p2ne, "90", ""), c.out)
 	}
-	// same but with equals and no percentile
+	// same but without percentile
 	for i, c := range cases {
-		cases[i] = Case{
-			strings.Replace(c.in, "=", "_is_", -1),
-			strings.Replace(strings.Replace(c.out, "=", "_is_", -1), "max_90", "max", 1),
-		}
+		cases[i].out = strings.Replace(c.out, "max_90", "max", 1)
 	}
 	for _, c := range cases {
-		assert.Equal(t, Max(c.in, "prefix.", "", ""), c.out)
+		assert.Equal(t, Max(c.in, c.p1, c.p2, c.p2ne, "", ""), c.out)
 	}
 }
 func TestRateCountPckt(t *testing.T) {
 	cases := []Case{
-		Case{"foo.bar.unit=yes.baz", "foo.bar.unit=Pckt.baz.orig_unit=yes.pckt_type=sent.direction=in"},
-		Case{"foo.bar.unit=yes", "foo.bar.unit=Pckt.orig_unit=yes.pckt_type=sent.direction=in"},
-		Case{"unit=yes.foo.bar", "unit=Pckt.foo.bar.orig_unit=yes.pckt_type=sent.direction=in"},
-		Case{"mtype=count.foo.unit=ok.bar", "mtype=count.foo.unit=Pckt.bar.orig_unit=ok.pckt_type=sent.direction=in"},
+		// metrics 2.0 cases with equals
+		Case{"foo.bar.unit=yes.baz", "prefix.", "", "ignored", "foo.bar.unit=Pckt.baz.orig_unit=yes.pckt_type=sent.direction=in"},
+		Case{"foo.bar.unit=yes", "prefix.", "our=prefix.", "ignored", "our=prefix.foo.bar.unit=Pckt.orig_unit=yes.pckt_type=sent.direction=in"},
+		Case{"unit=yes.foo.bar", "prefix.", "", "ignored", "unit=Pckt.foo.bar.orig_unit=yes.pckt_type=sent.direction=in"},
+		Case{"mtype=count.foo.unit=ok.bar", "prefix.", "", "ignored", "mtype=count.foo.unit=Pckt.bar.orig_unit=ok.pckt_type=sent.direction=in"},
+		// metrics 2.0 cases without equals
+		Case{"foo.bar.unit_is_yes.baz", "prefix.", "ignored", "", "foo.bar.unit_is_Pckt.baz.orig_unit_is_yes.pckt_type_is_sent.direction_is_in"},
+		Case{"foo.bar.unit_is_yes", "prefix.", "ignored", "our_is_prefix.", "our_is_prefix.foo.bar.unit_is_Pckt.orig_unit_is_yes.pckt_type_is_sent.direction_is_in"},
+		Case{"unit_is_yes.foo.bar", "prefix.", "ignored", "", "unit_is_Pckt.foo.bar.orig_unit_is_yes.pckt_type_is_sent.direction_is_in"},
+		Case{"mtype_is_count.foo.unit_is_ok.bar", "prefix.", "ignored", "", "mtype_is_count.foo.unit_is_Pckt.bar.orig_unit_is_ok.pckt_type_is_sent.direction_is_in"},
 	}
 	for _, c := range cases {
-		assert.Equal(t, CountPckt(c.in, "prefix."), c.out)
-		c = Case{
-			c.in,
-			strings.Replace(strings.Replace(c.out, "unit=Pckt", "unit=Pcktps", -1), "mtype=count", "mtype=rate", -1),
-		}
-		assert.Equal(t, RatePckt(c.in, "prefix."), c.out)
-	}
-	for _, c := range cases {
-		c = Case{
-			strings.Replace(c.in, "=", "_is_", -1),
-			strings.Replace(c.out, "=", "_is_", -1),
-		}
-		assert.Equal(t, CountPckt(c.in, "prefix."), c.out)
-		c = Case{
-			c.in,
-			strings.Replace(strings.Replace(c.out, "unit_is_Pckt", "unit_is_Pcktps", -1), "mtype_is_count", "mtype_is_rate", -1),
-		}
-		assert.Equal(t, RatePckt(c.in, "prefix."), c.out)
+		assert.Equal(t, CountPckt(c.in, c.p1, c.p2, c.p2ne), c.out)
+		c.out = strings.Replace(strings.Replace(c.out, "unit=Pckt", "unit=Pcktps", -1), "mtype=count", "mtype=rate", -1)
+		c.out = strings.Replace(strings.Replace(c.out, "unit_is_Pckt", "unit_is_Pcktps", -1), "mtype_is_count", "mtype_is_rate", -1)
+		assert.Equal(t, RatePckt(c.in, c.p1, c.p2, c.p2ne), c.out)
 	}
 }
 
 func BenchmarkDeriveCountsM20Bare(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		out = DeriveCount("foo=bar", "prefix-m1.", false)
+		out = DeriveCount("foo=bar", "prefix-m1.", "prefix-m2.", "prefix-m2ne.", false)
 	}
 }
 
 func BenchmarkDeriveCountsM20Proper(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		out = DeriveCount("foo=bar.unit=yes.mtype=count", "prefix-m1.", false)
+		out = DeriveCount("foo=bar.unit=yes.mtype=count", "prefix-m1.", "prefix-m2.", "prefix-m2ne.", false)
 	}
 }
 
 func BenchmarkDeriveCountsM20NoEqualsBare(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		out = DeriveCount("foo_is_bar", "prefix-m1.", false)
+		out = DeriveCount("foo_is_bar", "prefix-m1.", "prefix-m2.", "prefix-m2ne.", false)
 	}
 }
 
 func BenchmarkDeriveCountsM20NoEqualsProper(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		out = DeriveCount("foo_is_bar.unit_is_yes.mtype_is_count", "prefix-m1.", false)
+		out = DeriveCount("foo_is_bar.unit_is_yes.mtype_is_count", "prefix-m1.", "prefix-m2.", "prefix-m2ne.", false)
 	}
 }
