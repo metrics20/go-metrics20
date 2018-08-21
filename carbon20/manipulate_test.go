fc1a8481cbedbe0bdb2939ff878a555ed084efd2
Dieter Plaetinck <dieter@grafana.com>
2018-08-21 15:31:56 +0200

    find . -name '*.go' | xargs gofmt -w -s

--- carbon20/manipulate_test.go
+++ carbon20/manipulate_test.go
@@ -19,16 +19,16 @@ type Case struct {
 func TestDeriveCount(t *testing.T) {
 	cases := []Case{
 		// metrics 2.0 cases with equals
-		Case{"foo.bar.unit=yes.baz", "prefix.", "", "ignored", "foo.bar.unit=yesps.baz"},
-		Case{"foo.bar.unit=yes", "prefix.", "our=prefix.", "ignored", "our=prefix.foo.bar.unit=yesps"},
-		Case{"unit=yes.foo.bar", "prefix.", "", "ignored", "unit=yesps.foo.bar"},
-		Case{"mtype=count.foo.unit=ok.bar", "prefix.", "", "ignored", "mtype=rate.foo.unit=okps.bar"},
+		{"foo.bar.unit=yes.baz", "prefix.", "", "ignored", "foo.bar.unit=yesps.baz"},
+		{"foo.bar.unit=yes", "prefix.", "our=prefix.", "ignored", "our=prefix.foo.bar.unit=yesps"},
+		{"unit=yes.foo.bar", "prefix.", "", "ignored", "unit=yesps.foo.bar"},
+		{"mtype=count.foo.unit=ok.bar", "prefix.", "", "ignored", "mtype=rate.foo.unit=okps.bar"},
 
 		// metrics 2.0 cases without equals
-		Case{"foo.bar.unit_is_yes.baz", "prefix.", "ignored", "", "foo.bar.unit_is_yesps.baz"},
-		Case{"foo.bar.unit_is_yes", "prefix.", "ignored", "our_is_prefix.", "our_is_prefix.foo.bar.unit_is_yesps"},
-		Case{"unit_is_yes.foo.bar", "prefix.", "ignored", "", "unit_is_yesps.foo.bar"},
-		Case{"mtype_is_count.foo.unit_is_ok.bar", "prefix.", "ignored", "", "mtype_is_rate.foo.unit_is_okps.bar"},
+		{"foo.bar.unit_is_yes.baz", "prefix.", "ignored", "", "foo.bar.unit_is_yesps.baz"},
+		{"foo.bar.unit_is_yes", "prefix.", "ignored", "our_is_prefix.", "our_is_prefix.foo.bar.unit_is_yesps"},
+		{"unit_is_yes.foo.bar", "prefix.", "ignored", "", "unit_is_yesps.foo.bar"},
+		{"mtype_is_count.foo.unit_is_ok.bar", "prefix.", "ignored", "", "mtype_is_rate.foo.unit_is_okps.bar"},
 	}
 	for _, c := range cases {
 		assert.Equal(t, DeriveCount(c.in, c.p1, c.p2, c.p2ne, false), c.out)
@@ -39,15 +39,15 @@ func TestDeriveCount(t *testing.T) {
 func TestStat(t *testing.T) {
 	cases := []Case{
 		// metrics 2.0 cases with equals
-		Case{"foo.bar.unit=yes.baz", "prefix.", "", "ignored", "foo.bar.unit=yes.baz.stat=max_90"},
-		Case{"foo.bar.unit=yes", "prefix.", "our=prefix.", "ignored", "our=prefix.foo.bar.unit=yes.stat=max_90"},
-		Case{"unit=yes.foo.bar", "prefix.", "", "ignored", "unit=yes.foo.bar.stat=max_90"},
-		Case{"mtype=count.foo.unit=ok.bar", "prefix.", "", "ignored", "mtype=count.foo.unit=ok.bar.stat=max_90"},
+		{"foo.bar.unit=yes.baz", "prefix.", "", "ignored", "foo.bar.unit=yes.baz.stat=max_90"},
+		{"foo.bar.unit=yes", "prefix.", "our=prefix.", "ignored", "our=prefix.foo.bar.unit=yes.stat=max_90"},
+		{"unit=yes.foo.bar", "prefix.", "", "ignored", "unit=yes.foo.bar.stat=max_90"},
+		{"mtype=count.foo.unit=ok.bar", "prefix.", "", "ignored", "mtype=count.foo.unit=ok.bar.stat=max_90"},
 		// metrics 2.0 cases without equals
-		Case{"foo.bar.unit_is_yes.baz", "prefix.", "ignored", "", "foo.bar.unit_is_yes.baz.stat_is_max_90"},
-		Case{"foo.bar.unit_is_yes", "prefix.", "ignored", "our_is_prefix.", "our_is_prefix.foo.bar.unit_is_yes.stat_is_max_90"},
-		Case{"unit_is_yes.foo.bar", "prefix.", "ignored", "", "unit_is_yes.foo.bar.stat_is_max_90"},
-		Case{"mtype_is_count.foo.unit_is_ok.bar", "prefix.", "ignored", "", "mtype_is_count.foo.unit_is_ok.bar.stat_is_max_90"},
+		{"foo.bar.unit_is_yes.baz", "prefix.", "ignored", "", "foo.bar.unit_is_yes.baz.stat_is_max_90"},
+		{"foo.bar.unit_is_yes", "prefix.", "ignored", "our_is_prefix.", "our_is_prefix.foo.bar.unit_is_yes.stat_is_max_90"},
+		{"unit_is_yes.foo.bar", "prefix.", "ignored", "", "unit_is_yes.foo.bar.stat_is_max_90"},
+		{"mtype_is_count.foo.unit_is_ok.bar", "prefix.", "ignored", "", "mtype_is_count.foo.unit_is_ok.bar.stat_is_max_90"},
 	}
 	for _, c := range cases {
 		assert.Equal(t, Max(c.in, c.p1, c.p2, c.p2ne, "90", ""), c.out)
@@ -63,15 +63,15 @@ func TestStat(t *testing.T) {
 func TestRateCountPckt(t *testing.T) {
 	cases := []Case{
 		// metrics 2.0 cases with equals
-		Case{"foo.bar.unit=yes.baz", "prefix.", "", "ignored", "foo.bar.unit=Pckt.baz.orig_unit=yes.pckt_type=sent.direction=in"},
-		Case{"foo.bar.unit=yes", "prefix.", "our=prefix.", "ignored", "our=prefix.foo.bar.unit=Pckt.orig_unit=yes.pckt_type=sent.direction=in"},
-		Case{"unit=yes.foo.bar", "prefix.", "", "ignored", "unit=Pckt.foo.bar.orig_unit=yes.pckt_type=sent.direction=in"},
-		Case{"mtype=count.foo.unit=ok.bar", "prefix.", "", "ignored", "mtype=count.foo.unit=Pckt.bar.orig_unit=ok.pckt_type=sent.direction=in"},
+		{"foo.bar.unit=yes.baz", "prefix.", "", "ignored", "foo.bar.unit=Pckt.baz.orig_unit=yes.pckt_type=sent.direction=in"},
+		{"foo.bar.unit=yes", "prefix.", "our=prefix.", "ignored", "our=prefix.foo.bar.unit=Pckt.orig_unit=yes.pckt_type=sent.direction=in"},
+		{"unit=yes.foo.bar", "prefix.", "", "ignored", "unit=Pckt.foo.bar.orig_unit=yes.pckt_type=sent.direction=in"},
+		{"mtype=count.foo.unit=ok.bar", "prefix.", "", "ignored", "mtype=count.foo.unit=Pckt.bar.orig_unit=ok.pckt_type=sent.direction=in"},
 		// metrics 2.0 cases without equals
-		Case{"foo.bar.unit_is_yes.baz", "prefix.", "ignored", "", "foo.bar.unit_is_Pckt.baz.orig_unit_is_yes.pckt_type_is_sent.direction_is_in"},
-		Case{"foo.bar.unit_is_yes", "prefix.", "ignored", "our_is_prefix.", "our_is_prefix.foo.bar.unit_is_Pckt.orig_unit_is_yes.pckt_type_is_sent.direction_is_in"},
-		Case{"unit_is_yes.foo.bar", "prefix.", "ignored", "", "unit_is_Pckt.foo.bar.orig_unit_is_yes.pckt_type_is_sent.direction_is_in"},
-		Case{"mtype_is_count.foo.unit_is_ok.bar", "prefix.", "ignored", "", "mtype_is_count.foo.unit_is_Pckt.bar.orig_unit_is_ok.pckt_type_is_sent.direction_is_in"},
+		{"foo.bar.unit_is_yes.baz", "prefix.", "ignored", "", "foo.bar.unit_is_Pckt.baz.orig_unit_is_yes.pckt_type_is_sent.direction_is_in"},
+		{"foo.bar.unit_is_yes", "prefix.", "ignored", "our_is_prefix.", "our_is_prefix.foo.bar.unit_is_Pckt.orig_unit_is_yes.pckt_type_is_sent.direction_is_in"},
+		{"unit_is_yes.foo.bar", "prefix.", "ignored", "", "unit_is_Pckt.foo.bar.orig_unit_is_yes.pckt_type_is_sent.direction_is_in"},
+		{"mtype_is_count.foo.unit_is_ok.bar", "prefix.", "ignored", "", "mtype_is_count.foo.unit_is_Pckt.bar.orig_unit_is_ok.pckt_type_is_sent.direction_is_in"},
 	}
 	for _, c := range cases {
 		assert.Equal(t, CountPckt(c.in, c.p1, c.p2, c.p2ne), c.out)
